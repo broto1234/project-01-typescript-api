3313b6e5502df9697d6af0927483b38024080080
broto1234 <satyo97du@gmail.com>
2026-08-03 12:55:21 +0200

    Clean up Prisma config and ignore coverage

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,8 +3,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-console.log("Prisma DATABASE_URL:", process.env.DATABASE_URL);
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
